89b43608dcfb61063058d3141b5dbc4222805347
thang nguyen dinh <thangyk97@gmail.com>
2018-04-17 15:00:10 +0700

    a

---
+++ export_model.py
@@ -0,0 +1,264 @@
+from keras.models import Sequential, Model
+from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.merge import concatenate
+import matplotlib.pyplot as plt
+import keras.backend as K
+import tensorflow as tf
+import time
+import numpy as np
+import os, cv2
+from utils import WeightReader, decode_netout, draw_boxes, normalize
+#
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import utils
+from tensorflow.python.saved_model import tag_constants, signature_constants
+from tensorflow.python.saved_model.signature_def_utils_impl import build_signature_def, predict_signature_def
+from tensorflow.contrib.session_bundle import exporter
+#
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+# def get_session(gpu_fraction=0.8):
+#     '''Assume that you have 6GB of GPU memory and want to allocate ~2GB'''
+#
+#     num_threads = os.environ.get('OMP_NUM_THREADS')
+#     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+#
+#     if num_threads:
+#         return tf.Session(config=tf.ConfigProto(
+#             gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
+#     else:
+#         return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+#
+# K.set_session(get_session())
+
+LABELS = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+IMAGE_H, IMAGE_W = 416, 416
+GRID_H,  GRID_W  = 13 , 13
+BOX              = 5
+CLASS            = len(LABELS)
+CLASS_WEIGHTS    = np.ones(CLASS, dtype='float32')
+OBJ_THRESHOLD    = 0.1#0.5
+NMS_THRESHOLD    = 0.3#0.45
+ANCHORS          = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
+
+TRUE_BOX_BUFFER  = 50
+
+wt_path = '/home/pass/git/data/yolo.weights'
+train_image_folder = '/home/pass/git/data/train2014/'
+train_annot_folder = '/home/pass/git/data/train2014pascal/'
+valid_image_folder = '/home/pass/git/data/val2014/'
+valid_annot_folder = '/home/pass/git/data/val2014pascal/'
+
+
+# the function to implement the orgnization layer (thanks to github.com/allanzelener/YAD2K)
+def space_to_depth_x2(x):
+    return tf.space_to_depth(x, block_size=2)
+
+input_image = Input(shape=(IMAGE_H, IMAGE_W, 3))
+true_boxes  = Input(shape=(1, 1, 1, TRUE_BOX_BUFFER , 4))
+
+# Layer 1
+x = Conv2D(32, (3,3), strides=(1,1), padding='same', name='conv_1', use_bias=False)(input_image)
+x = BatchNormalization(name='norm_1')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 2
+x = Conv2D(64, (3,3), strides=(1,1), padding='same', name='conv_2', use_bias=False)(x)
+x = BatchNormalization(name='norm_2')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 3
+x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_3', use_bias=False)(x)
+x = BatchNormalization(name='norm_3')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 4
+x = Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_4', use_bias=False)(x)
+x = BatchNormalization(name='norm_4')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 5
+x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_5', use_bias=False)(x)
+x = BatchNormalization(name='norm_5')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 6
+x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_6', use_bias=False)(x)
+x = BatchNormalization(name='norm_6')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 7
+x = Conv2D(128, (1,1), strides=(1,1), padding='same', name='conv_7', use_bias=False)(x)
+x = BatchNormalization(name='norm_7')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 8
+x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_8', use_bias=False)(x)
+x = BatchNormalization(name='norm_8')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 9
+x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_9', use_bias=False)(x)
+x = BatchNormalization(name='norm_9')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 10
+x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_10', use_bias=False)(x)
+x = BatchNormalization(name='norm_10')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 11
+x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_11', use_bias=False)(x)
+x = BatchNormalization(name='norm_11')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 12
+x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_12', use_bias=False)(x)
+x = BatchNormalization(name='norm_12')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 13
+x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_13', use_bias=False)(x)
+x = BatchNormalization(name='norm_13')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+skip_connection = x
+
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 14
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_14', use_bias=False)(x)
+x = BatchNormalization(name='norm_14')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 15
+x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_15', use_bias=False)(x)
+x = BatchNormalization(name='norm_15')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 16
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_16', use_bias=False)(x)
+x = BatchNormalization(name='norm_16')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 17
+x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_17', use_bias=False)(x)
+x = BatchNormalization(name='norm_17')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 18
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_18', use_bias=False)(x)
+x = BatchNormalization(name='norm_18')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 19
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_19', use_bias=False)(x)
+x = BatchNormalization(name='norm_19')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 20
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_20', use_bias=False)(x)
+x = BatchNormalization(name='norm_20')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 21
+skip_connection = Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_21', use_bias=False)(skip_connection)
+skip_connection = BatchNormalization(name='norm_21')(skip_connection)
+skip_connection = LeakyReLU(alpha=0.1)(skip_connection)
+skip_connection = Lambda(space_to_depth_x2)(skip_connection)
+
+x = concatenate([skip_connection, x])
+
+# Layer 22
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_22', use_bias=False)(x)
+x = BatchNormalization(name='norm_22')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 23
+x = Conv2D(BOX * (4 + 1 + CLASS), (1,1), strides=(1,1), padding='same', name='conv_23')(x)
+output = Reshape((GRID_H, GRID_W, BOX, 4 + 1 + CLASS))(x)
+
+# small hack to allow true_boxes to be registered when Keras build the model
+# for more information: https://github.com/fchollet/keras/issues/2790
+# output = Lambda(lambda args: args[0])([output, true_boxes])
+
+model = Model(input_image, output)
+
+# Print construct of model
+model.summary()
+
+
+
+model.load_weights("weights_coco.h5")
+
+dummy_array = np.zeros((1,1,1,1,TRUE_BOX_BUFFER,4))
+
+####### Export Model ##########
+export_path = '/home/vtc/Desktop/model/mnist_model'
+builder = saved_model_builder.SavedModelBuilder(export_path)
+print(model.input)
+signature = predict_signature_def(
+    inputs  = {'images': model.input},
+    outputs = {'boxs'  : model.output} )
+
+with K.get_session() as sess:
+    builder.add_meta_graph_and_variables(
+        sess = sess,
+        tags=[tag_constants.SERVING],
+        signature_def_map={'predict': signature})
+    builder.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #
+# def test_img(link_img):
+#     start = time.time()
+#     image = cv2.imread(link_img)
+#
+#     # plt.figure(figsize=(10, 10))
+#
+#     input_image = cv2.resize(image, (416, 416))
+#     input_image = input_image / 255.
+#     input_image = input_image[:, :, ::-1]
+#     input_image = np.expand_dims(input_image, 0)
+#
+#     netout = model.predict([input_image, dummy_array])
+#     print(str(netout.shape))
+#     boxes = decode_netout(netout[0],
+#                           obj_threshold=OBJ_THRESHOLD,
+#                           nms_threshold=NMS_THRESHOLD,
+#                           anchors=ANCHORS,
+#                           nb_class=CLASS)
+#     image = draw_boxes(image, boxes, labels=LABELS)
+#     print("{} s".format(time.time() - start))
+#     plt.imshow(image[:, :, ::-1]);
+#     plt.show()
+#
+# k = 'lol'
+# while 1:
+#     k = input("Nhap link anh('q' de thoat)")
+#     if k == 'q' or k == 'Q':
+#         break
+#     try:
+#         test_img(k)
+#     except:
+#         pass
+
+
